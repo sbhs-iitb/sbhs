000f50a2ae71d6e938ab7841fdf8ccbc2fedd3c2
coderick14 <bhattacharyya.rick14@gmail.com>
2017-06-30 01:43:51 +0530

    Fetch logs from RPis during cronjob

--- sbhs_server/scan_rpis.sh
+++ sbhs_server/scan_rpis.sh
@@ -5,15 +5,16 @@ cd sbhs_server
 mkdir -p RPi_data
 mkdir -p RPi_data/map RPi_data/report
 
-# get IP addresses of connected Raspberry Pi's
-nmap -n -sP 10.42.0.255/24 | grep -E -o "(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])" | grep -v "10.42.0.1" > RPi_data/ipaddrs.txt
-
 # Get map_machine_ids.txt and server reports from all connected Raspberry Pi's
 while read -r ip
 do
-	curl -o RPi_data/map/$ip.txt $ip/map_machine_ids.txt
-	curl -o RPi_data/report/$ip.txt $ip/report.json
+    echo $ip
+    curl -o RPi_data/map/$ip.txt $ip/map_machine_ids.txt
+    curl -o RPi_data/report/$ip.txt $ip/report.json
+    rsync -arz --exclude="django_error.log" -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress pi@$ip:/home/pi/sbhs-pi/log/ /home/vlabs-sbhs/code/sbhs/log/
+    rsync -arz --remove-source-files -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress pi@$ip:/home/pi/sbhs-pi/experiments/ /home/vlabs-sbhs/code/sbhs/experiments/
 done < "RPi_data/ipaddrs.txt"
 
 # Execute script to create IP map
 python create_ip_map.py
+
